ef07c5acf3b9e2c8a6231521a877e40b0bb7bc45
Newton Zheng <newtonzheng624@gmail.com>
2019-03-28 11:11:58 -0400

    store user input javascript

--- static/js/primary.js
+++ static/js/primary.js
@@ -2,3 +2,12 @@
 
 //When user clicks on the submit button, the following code will run
 $("#output1").on("click", function(){
+
+  //Takes the input from the user from the input and store in a variable
+  var user_input = $("#exam_selection").val();
+
+  //return false if user input is null
+  if (user_input.length <= 0) return false;
+
+  //If user wants exams, then this function runs
+  if (user_input == "Exam")
