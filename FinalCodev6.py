2eb0df38c58bbb91da55b78ab39335b136fd19a3
Newton Zheng <newtonzheng624@gmail.com>
2019-04-07 16:10:16 -0400

    modified code and added documentation

--- FinalCodev6.py
+++ FinalCodev6.py
@@ -2,7 +2,7 @@
 
 import os
 from flask import Flask, jsonify, request
-from urllib import request as req
+#from urllib import request as req
 
 #Imports the Discovery Functionality
 from watson_developer_cloud import DiscoveryV1
@@ -28,147 +28,147 @@ def process():
 
     #The full list of 22 sample courses used in IBM Watson Discovery. If the query runs and the following course IDs are found in the PDFs of the query, then it returns just the ID of the course rather than the content of the entire PDF
     course_list = ['ANAT 100', 'ARTH 250', 'BCHM 218', 'BIOL 102', 'BIOL 103', 'BIOL 350', 'CISC 101', 'COGS 100', 'DEVS 100', 'ENIN 140', 'FILM 240', 'FILM 260', 'GNDS 120', 'GNDS 215', 'HIST 125', 'HIST 263', 'HIST 270', 'HLTH 200', 'HLTH 230', 'MATH 121', 'PHGY 170', 'PSYC 205']
-
+    
     #Queries for courses with NO EXAMS AND GROUPS
-    if (request.form['exam'] == 'no' and request.form['name'] == 'no'):
-        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:!proctored, group'))
+    if (request.form['exam'] == 'no' and request.form['name'] == 'no'): 
+        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:!proctored, group')) 
 
         filtered_course_list = []
 
         for course_name in course_list:
             if course_name in my_course_query:
                 filtered_course_list.append(course_name)
-
+        
          #Return the course results and the appropriate message to the HTML front end
 
         return jsonify({
             'course_output' : filtered_course_list,
             'text_output' : 'SUCCESS! The following courses have NO EXAMS and NO GROUP WORK: ',
-
+        
         })
-
+    
     #Queries for courses with EXAMS AND GROUPS
-    elif (request.form['exam'] == 'yes' and request.form['name'] == 'yes'):
-        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:final, exam, proctored, group'))
+    elif (request.form['exam'] == 'yes' and request.form['name'] == 'yes'): 
+        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:final, exam, proctored, group')) 
 
         filtered_course_list = []
 
         for course_name in course_list:
             if course_name in my_course_query:
                 filtered_course_list.append(course_name)
-
+        
         return jsonify({
             'course_output' : filtered_course_list,
             'text_output' : 'SUCCESS! The following courses have BOTH EXAMS and GROUP WORK: ',
-
+        
         })
-
+    
     #QUERY for courses with GROUP WORK BUT NO EXAMS
-
-    elif (request.form['exam'] == 'no' and request.form['name'] == 'yes'):
-        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:!"proctored",text:"group"'))
+    
+    elif (request.form['exam'] == 'no' and request.form['name'] == 'yes'): 
+        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:!"proctored",text:"group"')) 
 
         filtered_course_list = []
 
         for course_name in course_list:
             if course_name in my_course_query:
                 filtered_course_list.append(course_name)
-
+        
         return jsonify({
             'course_output' : filtered_course_list,
             'text_output' : 'SUCCESS! The following courses have GROUP WORK BUT NO EXAMS: ',
-
+        
         })
 
     #QUERY for courses with EXAMS BUT NO GROUP WORK
-    elif (request.form['exam'] == 'yes' and request.form['name'] == 'no'):
-        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:"proctored",text:!"group"'))
+    elif (request.form['exam'] == 'yes' and request.form['name'] == 'no'): 
+        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:"proctored",text:!"group"')) 
 
         filtered_course_list = []
 
         for course_name in course_list:
             if course_name in my_course_query:
                 filtered_course_list.append(course_name)
-
+        
         return jsonify({
             'course_output' : filtered_course_list,
             'text_output' : 'SUCCESS! The following courses have EXAMS BUT NO GROUP WORK: ',
-
+        
         })
+    
+    
+    
+    
+    #Queries for courses with NO EXAMS 
 
-
-
-
-    #Queries for courses with NO EXAMS
-
-    elif request.form['exam'] == 'no':
-        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:!proctored'))
+    elif request.form['exam'] == 'no': 
+        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:!proctored')) 
 
         filtered_course_list = []
 
         for course_name in course_list:
             if course_name in my_course_query:
                 filtered_course_list.append(course_name)
-
+        
         return jsonify({
             'course_output' : filtered_course_list,
             'text_output' : 'SUCCESS! The following courses have NO EXAMS: ',
-
+        
         })
-
+    
     #Queries for courses with EXAMS
 
-    elif request.form['exam'] == 'yes':
-        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:final, exam, proctored'))
+    elif request.form['exam'] == 'yes': 
+        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:final, exam, proctored')) 
 
         filtered_course_list = []
 
         for course_name in course_list:
             if course_name in my_course_query:
                 filtered_course_list.append(course_name)
-
+        
         return jsonify({
             'course_output' : filtered_course_list,
             'text_output' : 'SUCCESS! The following courses have EXAMS: ',
-
+        
         })
 
-
+    
     # Queries for courses with GROUP WORK
-    elif request.form['name'] == 'yes':
-        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:group'))
+    elif request.form['name'] == 'yes': # Corresponses with line 52 in index.html
+        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:group')) 
 
         filtered_course_list = []
 
         for course_name in course_list:
             if course_name in my_course_query:
                 filtered_course_list.append(course_name)
-
+        
         return jsonify({
             'course_output' : filtered_course_list,
             'text_output' : 'SUCCESS! The following courses have GROUP WORK: ',
-
+        
         })
-
-
+    
+    
     # Queries for courses with NO GROUP WORK
     elif request.form['name'] == 'no':
-        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:!group'))
+        my_course_query = str(discovery.query('812fbe50-c0d5-41dc-80c9-77fc6563cb40', 'b213e90d-6df5-4f68-9456-5ba78da80be1', 'text:!group')) 
 
         filtered_course_list = []
 
         for course_name in course_list:
             if course_name in my_course_query:
                 filtered_course_list.append(course_name)
-
+        
         return jsonify({
             'course_output' : filtered_course_list,
             'text_output' : 'SUCCESS! The following courses have NO GROUP WORK: ',
-
+        
         })
     else:
-        return jsonify({'error' : 'Error: No preferences selected. Try again!'})
-
+        return jsonify({'error' : 'Error: No preferences selected. Try again!'})    
+         
 #END OF SEARCH QUERIES
 
 port = os.getenv('PORT', '5020')
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # comm493-analytics-ai-project
 Final COMM493 Analytics and AI for Business Project
+
+[Video Demo Link](https://youtu.be/aSixoxLwBRk)
+
+There are two ways to run the code below:
+1. Run using JupyterLab
+2. Run using localhost
+
+# To run in JupyterLab
+cd the folder that contains all the coding files in this Github repository
+Change the port in FinalCodev6.py to the appropriate port for your JupyterLab
+
+Then run the following code:
+
+'''
+python3 FinalCodev6.py
+
+'''
+
+# To run locally
+
+In FinalCodev6.py, change 'from watson_developer_cloud import DiscoveryV1' to 'from ibm_watson import DiscoveryV1'
+
+Run the following code in command terminal to set up the Python virtual environment, flask, and IBM Watson:
+
+'''
+python -m venv env
+env\Scripts\activate
+pip install flask
+pip install --upgrade "ibm-watson>=3.0.3"
+'''
+
+If you already have a virtual Python environment set up with Flask and IBM Watson installed, you can run the following code:
+
+'''
+set FLASK_APP=FinalCodev6.py
+flask run
+
+'''
+
+Open localhost:5000 (http://127.0.0.1:5000/) on your browser to launch program
+
+Control C to exit program in command terminal
